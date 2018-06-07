6ee7a45503e6d43a8fcf4db2fdfc17088276a4f6
kevin <kevin.riedl.privat@gmail.com>
2018-06-07 19:26:25 +0200

    wget

--- example/run.sh
+++ example/run.sh
@@ -1,7 +1,10 @@
+
+
 while true;
 do
     echo Sending IR signal..
-    curl --data "code=16429347" http://192.168.1.28/ir
+    wget -q --spider http://192.168.1.28/ir?code=16429347
+    #curl --data "code=16429347" http://192.168.1.28/ir
     echo IR Signal sent
     sleep 5
 done
