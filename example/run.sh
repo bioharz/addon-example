f3eee291cb6034262983b8d73111adbf2c929c4e
kevin <kevin.riedl.privat@gmail.com>
2018-06-07 20:01:01 +0200

    hh

--- example/run.sh
+++ example/run.sh
@@ -1,13 +1,15 @@
 
 main() {
+	CONFIG_PATH=/data/options.json
+
 
 	while true;
 	do
 		echo Sending IR signal..
-		wget -q --spider http://192.168.1.28/ir?code=$(hass.config.get 'ir_signal')
+		wget -q --spider http://192.168.1.28/ir?code=$(jq --raw-output ".ir_signal" $CONFIG_PATH)
 		#curl --data "code=16429347" http://192.168.1.28/ir
 		echo IR Signal sent
-		sleep $(hass.config.get 'sending_interval_sec')
+		sleep $(jq --raw-output ".sending_interval_sec" $CONFIG_PATH)
 	done
 }
 
