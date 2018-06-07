4e07ae4875baaa8fc89c05291d740459a0182468
kevin <kevin.riedl.privat@gmail.com>
2018-06-07 19:36:06 +0200

    hh

--- example/run.sh
+++ example/run.sh
@@ -1,10 +1,13 @@
 
+main() {
+	while true;
+	do
+		echo Sending IR signal..
+		wget -q --spider http://192.168.1.28/ir?code=$(hass.config.get 'ir_signal')
+		#curl --data "code=16429347" http://192.168.1.28/ir
+		echo IR Signal sent
+		sleep $(hass.config.get 'sending_interval_sec')
+	done
+}
 
-while true;
-do
-    echo Sending IR signal..
-    wget -q --spider http://192.168.1.28/ir?code=16429347
-    #curl --data "code=16429347" http://192.168.1.28/ir
-    echo IR Signal sent
-    sleep 5
-done
+main "$@"
